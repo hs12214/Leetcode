01aa192bf36caa64795895c9bada636c6956149c
Harshit Sharma <hsharma6002@gmail.com>
2026-08-26 11:08:57 +0530

    Time: 0 ms (100%), Space: 8.6 MB (37.92%) - LeetHub

---
+++ 2520-count-the-digits-that-divide-a-number/2520-count-the-digits-that-divide-a-number.c
@@ -0,0 +1,13 @@
+int countDigits(int num) {
+    int temp = num;
+    int count = 0;
+    while (temp > 0) {
+        int digit = temp % 10;
+        if (digit != 0 && num % digit == 0) {
+            count++;
+        }
+        temp /= 10;
+    }
+    return count;
+}
+
