bae8becfd14bf047535d26c96a75a83c0ecd2f60
Harshit Sharma <hsharma6002@gmail.com>
2026-08-30 19:15:46 +0530

    Time: 3 ms (36.39%), Space: 8.9 MB (22.33%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.c
@@ -0,0 +1,9 @@
+bool isPalindrome(int x) {
+    if(x < 0) return false;
+    long rev = 0, orig = x;
+    while(x > 0){
+        rev = rev*10 + x%10;
+        x /= 10;
+    }
+    return rev == orig;
+}
