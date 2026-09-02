be0cadf71fb70089ae5212026e707f7480d05e5f
Harshit Sharma <hsharma6002@gmail.com>
2026-09-02 11:09:32 +0530

    Time: 0 ms (100%), Space: 14.4 MB (59.51%) - LeetHub

---
+++ 0905-sort-array-by-parity/0905-sort-array-by-parity.c
@@ -0,0 +1,20 @@
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int* sortArrayByParity(int* nums, int numsSize, int* returnSize){
+    int i,j=numsSize-1;
+    while(i<j){
+        if(nums[i]%2==0) i++;
+        else if(nums[j]%2==1) j--;
+        else{
+            int temp=nums[i];
+            nums[i]=nums[j];
+            nums[j]=temp;
+            i++;
+            j--;
+        }
+    }
+    *returnSize=numsSize;
+    return nums;
+}
+
